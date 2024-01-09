d4a771546737ca689f17c69234da2940906a2a08
2658308290 <2658308290@qq.com>
2024-01-09 11:07:39 +0800

    学校角色侧边栏第一个完成

--- src/pages/main/routes/planManage/newEvent/index.vue
+++ src/pages/main/routes/planManage/newEvent/index.vue
@@ -34,55 +34,84 @@
       </div>
 
       <div class="upDepartment row">
-        <div class="title">培训主题</div>
-        <el-select
-          v-model="value"
-          placeholder="请选择"
-          :style="{ width: '510px' }"
-        >
-          <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          >
-          </el-option>
-        </el-select>
-      </div>
-
-      <div class="trianTime row">
-        <div class="title">培训时间</div>
-        <div class="box">
-          <div>
-            <el-select
-              v-model="startTimeValue"
-              placeholder="请选择培训开始时间"
+        <div class="title" style="margin-top: 3px;">培训类型</div>
+        <div class="chooseBox">
+          <div class="up">
+            <el-radio v-model="btn_choose" label="provience"
+              >省级及以上培训</el-radio
             >
-              <el-option
-                v-for="item in startTimeOption"
-                :key="item.value"
-                :value="item.value"
-                :label="item.label"
-              >
-              </el-option>
-            </el-select>
+            <el-radio v-model="btn_choose" label="college"
+              >校级赋能培训</el-radio
+            >
+            <el-radio v-model="btn_choose" label="school"
+              >院级强基培训</el-radio
+            >
           </div>
-          <div class="last">至</div>
-          <div>
-            <el-select v-model="endTimeValue" placeholder="请选择培训开始时间">
-              <el-option
-                v-for="item in endTimeOption"
-                :key="item.value"
-                :value="item.value"
-                :label="item.label"
+          <!-- <div class="btm">
+            <el-input
+              placeholder="请输入校内培训地点"
+              :style="{ width: '510px' }"
+            >
+            </el-input>
+          </div> -->
+        </div>
+      </div>
+      <div v-if="btn_choose === 'school'" class="theme row">
+        <div class="title">课程内容</div>
+        <el-input
+          v-model="input1"
+          type="textarea"
+          rows="5"
+          placeholder="请简述主题说明"
+          :style="{ width: '510px' }"
+        />
+      </div>
+      <div class="trianTime row">
+        <div class="title" style="margin-top: 3px;">培训时间</div>
+        <div class="box">
+          <div class="up">
+            <div>
+              <el-select
+                v-model="startTimeValue"
+                placeholder="请选择培训开始时间"
               >
-              </el-option>
-            </el-select>
+                <el-option
+                  v-for="item in startTimeOption"
+                  :key="item.value"
+                  :value="item.value"
+                  :label="item.label"
+                >
+                </el-option>
+              </el-select>
+            </div>
+            <div class="last">至</div>
+            <div>
+              <el-select
+                v-model="endTimeValue"
+                placeholder="请选择培训开始时间"
+              >
+                <el-option
+                  v-for="item in endTimeOption"
+                  :key="item.value"
+                  :value="item.value"
+                  :label="item.label"
+                >
+                </el-option>
+              </el-select>
+            </div>
+          </div>
+          <div v-if="btn_choose === 'provience'" class="down">
+            <div style="color: #FD2626;">*</div>
+            系统默认站内信通知指定辅导员
+          </div>
+          <div v-if="btn_choose === 'college'" class="down">
+            <div style="color: #FD2626;">*</div>
+            系统默认培训开始前一天站内信通知确认参加本次培训的辅导员。
           </div>
         </div>
       </div>
-      <div class="trianAdress row">
-        <div class="title">
+      <div v-if="btn_choose != 'school'" class="trianAdress row">
+        <div class="title" style="margin-top: 5px;">
           培训地点
         </div>
 
@@ -100,8 +129,30 @@
           </div>
         </div>
       </div>
+      <div v-if="btn_choose === 'college'" class="theme row">
+        <div class="title">课程内容</div>
+        <el-input
+          v-model="input1"
+          type="textarea"
+          rows="5"
+          placeholder="请简述课程内容"
+          :style="{ width: '510px' }"
+        />
+      </div>
+
+      <div v-if="btn_choose != 'school'" class="theme row">
+        <div class="title">主讲人</div>
+        <el-input
+          v-model="input1"
+          placeholder="请输入主讲人姓名"
+          :style="{ width: '510px' }"
+        />
+      </div>
       <div class="addTeacher row">
-        <div class="title">添加辅导员</div>
+        <div class="title">
+          <div v-if="btn_choose != 'school'">添加辅导员</div>
+          <div v-else>添加学院学生工作负责人</div>
+        </div>
         <div>
           <div class="up">
             <el-radio v-model="add" label="勾选添加"></el-radio>
@@ -192,6 +243,17 @@
           </div>
         </div>
       </div>
+      <div v-if="btn_choose === 'provience'" class="row">
+        <div class="title">
+          培训证明&课程评价截止上传时间
+        </div>
+        <div class="adressBox">
+          <div class="up">
+            <el-radio v-model="add2" label="勾选添加"></el-radio>
+            <el-radio v-model="add2" label="导入模板添加"></el-radio>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -232,6 +294,8 @@ export default {
       input1: '',
       in_out: '校内培训',
       add: '勾选添加',
+      add2: '勾选添加',
+      btn_choose: 'provience',
 
       tableData: [
         {
@@ -323,7 +387,7 @@ export default {
       display: flex;
       flex-direction: row;
       align-items: center;
-      width: 200px;
+      width: 250px;
     }
     .row {
       display: flex;
@@ -334,30 +398,50 @@ export default {
       color: #000000e6;
     }
     .theme {
+      display: flex;
+      flex-direction: row;
+      align-items: start;
       .title {
       }
     }
     .upDepartment {
+      display: flex;
+      flex-direction: row;
+      align-items: start;
       .title {
+      }
+      .chooseBox {
+        display: flex;
+        flex-direction: column;
       }
     }
     .trianTime {
+      display: flex;
+      flex-direction: row;
+      align-items: start;
       .title {
       }
       .box {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        width: 510px;
-        div {
+        .up {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+          width: 510px;
+          .last {
+            font-weight: 500;
+            font-size: 14px;
+            color: #000000e6;
+            line-height: 22px;
+          }
         }
-
-        .last {
-          font-weight: 500;
-          font-size: 14px;
-          color: #000000e6;
-          line-height: 22px;
+        .down {
+          margin-top: 8px;
+          display: flex;
+          flex-direction: row;
+          color: #7a7a7a;
+          font-size: 12px;
+          font-weight: 400;
         }
       }
     }
@@ -472,6 +556,10 @@ export default {
       }
     }
   }
+}
+/deep/.el-textarea__inner {
+  padding: 5px 10px;
+  resize: none;
 }
 /deep/ .top_right .el-button {
   padding: 0px 0px;
